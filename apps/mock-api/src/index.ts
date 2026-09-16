3755e8457726aba80fc4ebb36b1a00c773358a58
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2026-09-16 18:59:17 +1200

    feat: post read-later item

--- apps/mock-api/src/index.ts
+++ apps/mock-api/src/index.ts
@@ -3,7 +3,8 @@ import cors from "cors";
 import { readFile } from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import type { ArticlesResponse, ReadLaterResponse } from "shared-core";
+import type { ArticlesResponse, ReadLaterItem, ReadLaterResponse } from "shared-core";
+import { writeFile } from "fs/promises";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -56,6 +57,29 @@ app.get("/read-later", async (_req, res) => {
   }
 });
 
+app.post("/read-later", async (_req, res) => {
+  try {
+    const readLaterId = _req.body.articleId;
+    const savedAt = new Date().toISOString();
+    const newItem: ReadLaterItem = { articleId: readLaterId, savedAt };
+
+    const data = await readFile(readLaterPath, "utf-8");
+    const items: ReadLaterItem[] = JSON.parse(data);
+    if(items.some((item: ReadLaterItem) => item.articleId === readLaterId)) {
+      return res.status(409).json({ error: "Item already exists" });
+    }
+    items.push(newItem);
+
+    await writeFile(readLaterPath, JSON.stringify(items, null, 2), "utf-8");
+    res.status(201).json({ message: "Item added to read-later" });
+  }
+  catch {
+    res.status(500).json({
+      error: "Failed to add item to read-later",
+    });
+  }
+
+})
 
 app.listen(PORT, () => {
   console.log(`Mock API running on http://localhost:${PORT}`);
